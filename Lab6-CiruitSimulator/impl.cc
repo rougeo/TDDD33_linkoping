c0d7806af1deab1d5df68d14048a7cde7121c6c7
Michel Jeremy <micje093@student.liu.se>
2016-11-25 09:56:48 +0100

    cleaned up

--- Lab6-CiruitSimulator/impl.cc
+++ Lab6-CiruitSimulator/impl.cc
@@ -1,53 +1,87 @@
 #include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+class ConnectionPoint {
+  public:
+    ConnectionPoint(){}
+
+    void setPotential(double value) {
+      potential = value;
+    }
+
+    double getPotential() {
+      return potential;
+    }
+private:
+  double potential = 0;
+
+};
+
 
 class Component {
   public:
-    // constructor
-
-
-    virtual void moveVoltage(int& terminal0, int& terminal1, double exec_time) = 0;
-
-    double getVoltage() {
-      return abs(terminal0 - terminal1);
+    Component(string pname) {
+      name = pname;
     }
+    //virtual void moveVoltage(int& terminal0, int& terminal1, double exec_time) = 0;
 
-    virtual double getIntensity() = 0;
-    virtual double getCurrent() = 0;
+    // double getVoltage() {
+    //   return abs(terminal0 - terminal1);
+    // }
+
+    //virtual double getIntensity() = 0;
+    //virtual double getCurrent() = 0;
 
 
-  private:
+  protected:
     double terminal0;
     double terminal1;
     double voltage;
     double current;
+    string name;
+    ConnectionPoint cp1;
+    ConnectionPoint cp2;
 };
 
 class Battery : public Component {
   public:
-    Battery(string name, double voltage, ConnectionPoint cp1, ConnectionPoint cp2) {
-      name = name;
-      voltage = getVoltage();
-      cp1 = setPotential(max(terminal0, terminal1));
-      cp2 = setPotential(min(terminal0, terminal1));
+    Battery(string pname, ConnectionPoint& cp1, ConnectionPoint& cp2, double pvoltage)
+      : Component(pname) {
+        voltage = pvoltage;
+        terminal0 = pvoltage;
+        terminal1 = 0;
+        cp1.setPotential(pvoltage);
+        cp2.setPotential(0);
     }
 
 
-    double getIntensity() override {
-
-    }
-  private:
-    string name;
+    // double getIntensity() override {
+    //   // TODO
+    //   return 0.0;
+    // }
 };
 
 class Resistor : public Component {
   public:
-  void moveVoltage(int& terminal0, int& terminal1, double exec_time) override {
-    terminal1 = (terminal0 / resistance) * exec_time;
-  }
+    Resistor(string pname, ConnectionPoint cp1, ConnectionPoint cp2, double presistance)
+      : Component(pname) {
+        resistance = presistance;
+        terminal0 = cp1.getPotential();
+        terminal1 = cp2.getPotential();
+      }
 
-  double getIntensity() override {
-    return (getVoltage() / resistance);
-  }
+  // void moveVoltage(int& terminal0, int& terminal1, double exec_time) override {
+  //   terminal1 = (terminal0 / resistance) * exec_time;
+  // }
+
+  // double getIntensity() override {
+  //   return (getVoltage() / resistance);
+  // }
 
   private:
     double resistance;
@@ -55,34 +89,45 @@ class Resistor : public Component {
 
 class Capacitor : public Component {
   public:
-    void moveVoltage(int& terminal0, int& terminal1, double exec_time) override {
-
-    }
-
-    double getIntensity() override {
-
-    }
+    Capacitor(string pname, ConnectionPoint cp1, ConnectionPoint cp2, double pcapacity)
+      : Component(pname) {
+        capacity = pcapacity;
+        terminal0 = cp1.getPotential();
+        terminal1 = cp2.getPotential();
+      }
+    // void moveVoltage(int& terminal0, int& terminal1, double exec_time) override {
+    //  // TODO
+    // }
+    //
+    // double getIntensity() override {
+    //   // TODO
+    //   return 0.0;
+    // }
   private:
-};
-
-class ConnectionPoint {
-  public:
-    ConnectionPoint(){}
-
-    setPotential(double value) {
-      potential = value;
-    }
-private:
-  double potential;
-
+    double capacity;
 };
 
 
-void simulate(...) {
 
+void deallocate_component(vector<Component*> vec) {
+  while (!vec.empty()) {
+    vec.pop_back();
+  }
+}
+
+void simulate(vector<Component*> net, double simul_time, int lines, double step) {
+  for (double time = 0; time < simul_time; time += step) {
+    // EACH SIMULATION STEP BY STEP
+    // COUT's
+  }
 }
 
 int main() {
-  // init components and connections
-  // call simulate
+  ConnectionPoint cp1, cp2;
+  vector<Component*> net;
+  net.push_back(new Battery("Bat1", cp1, cp2, 24.0));
+  net.push_back(new Resistor("R1", cp1, cp2, 6.0));
+  net.push_back(new Resistor("R2", cp1, cp2, 8.0));
+  simulate(net, 10000, 10, 0.1);
+  deallocate_component(net);
 }
